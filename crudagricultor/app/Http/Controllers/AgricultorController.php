79bc2f15de7a6fc95e3deb3e56c24cc6fd378c34
William Jimenez <williamrjimenez2003@gmail.com>
2022-04-24 22:14:18 -0600

    Examen Parcial II y prueba

---
+++ crudagricultor/app/Http/Controllers/AgricultorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\agricultor;
+use Illuminate\Http\Request;
+
+class AgricultorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\agricultor  $agricultor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(agricultor $agricultor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\agricultor  $agricultor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(agricultor $agricultor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\agricultor  $agricultor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, agricultor $agricultor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\agricultor  $agricultor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(agricultor $agricultor)
+    {
+        //
+    }
+}
